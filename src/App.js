8e9a2de781140e9fd988efa4b7c957676b2b0fc4
thathva <34968163+thathva@users.noreply.github.com>
2021-10-30 17:45:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles.js";
+import wordsToNumbers from "words-to-numbers";
 
 function App() {
+  const classes = useStyles();
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const [newsArticles, setNewsArticles] = useState([]);
+  useEffect(() => {
+    alanBtn({
+      key: "fd259844b507d3bf1939f8e1114379c72e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveActicle) => prevActiveActicle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again later");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening");
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className={classes.logoContainer}>
+        <img
+          src="https://iotbusinessnews.com/WordPress/wp-content/uploads/artificial-intelligence-AI.jpg"
+          className={classes.alanLogo}
+          alt="alan"
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 }
